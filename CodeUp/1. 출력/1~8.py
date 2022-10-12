0b5e7171c2884670647f60ef66676159fc14b0a2
Dragonkeun <dragonkeun7@gmail.com>
2022-10-12 23:00:22 +0900

    feat: CodeUp 18번까지 완료

---
+++ CodeUp/1. 출력/1~8.py
@@ -0,0 +1,32 @@
+# 1. Hello 출력하기
+print("Hello")
+
+# 2. Hello World 출력하기
+print("Hello World")
+
+# 3. Hello
+# World 두 줄에 걸쳐 줄을 바꿔 출력하기
+# 작은따옴표, 큰따옴표를 3개씩 사용하면 입력하는대로 출력된다
+print("""Hello
+      World""")
+
+# 4. 'Hello' 출력하기
+print("'Hello'")
+
+# 5. "Hello World" 출력하기
+print('"Hello World"')
+
+# 6. "!@#$%^&*()" 출력하기
+print('"!@#$%^&*()"')
+
+# 7. "C:\Download\hello.cpp" 출력하기
+print('"C:\Download\hello.cpp"')
+
+# 8. ┌┬┐ 출력하기
+#    ├┼┤
+#    └┴┘
+print("""
+        ┌┬┐
+        ├┼┤
+        └┴┘
+      """)
