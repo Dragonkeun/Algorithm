f868ae1824a201267a3fb2723b837b8706ca78b3
Dragonkeun <dragonkeun7@gmail.com>
2022-11-08 21:54:23 +0900

    CodeUp ~55

--- CodeUp/8. 논리연산/53~55.py
+++ CodeUp/8. 논리연산/53~55.py
@@ -16,3 +16,9 @@ print(not a)
 a, b = map(int, input().split())
 if a and b :
     print("참")
+
+# 55. 두 개의 참(1) 또는 거짓(0)이 입력될 때, 하나라도 참이면 참을 출력하는 프로그램을 작성해보자.
+a, b = map(int, input().split())
+if a or b :
+    print("참")
+
