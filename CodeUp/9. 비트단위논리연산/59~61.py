382dab601886196d14975e2449c22c9ceafb52f3
Dragonkeun <dragonkeun7@gmail.com>
2022-11-13 14:49:57 +0900

    CodeUp ~60

--- CodeUp/9. 비트단위논리연산/59~61.py
+++ CodeUp/9. 비트단위논리연산/59~61.py
@@ -11,3 +11,7 @@ print(~a)
 # 60. 입력된 정수 두 개를 비트단위로 and 연산한 후 그 결과를 정수로 출력해보자.
 a, b = map(int, input().split())
 print(a & b)
+
+# 61. 입력된 정수 두 개를 비트단위로 or 연산한 후 그 결과를 정수로 출력해보자.
+a, b = map(int, input().split())
+print(a | b)
