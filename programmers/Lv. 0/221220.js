596ab73bdafa5b319ea52f620819a5b795715d7e
Dragonkeun <Dragonkeun7@gmail.com>
2022-12-20 23:56:50 +0900

    programmers 221220

---
+++ programmers/Lv. 0/221220.js
@@ -0,0 +1,10 @@
+// A로 B 만들기
+function solution(before, after) {
+    before = before.split("")
+    for(let i=0; i<before.length; i++){
+        if (after.indexOf(before[i])){
+            var result = 1
+        }
+    }
+    return result;
+}
